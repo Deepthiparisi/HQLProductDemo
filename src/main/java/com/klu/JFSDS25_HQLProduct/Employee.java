81b8d375d44f2bf14b3c988d0b28e383fe8b37bc
Deepthiparisi <deepthiparisi18@gmail.com>
2024-12-07 12:39:49 +0530

    V1.0

---
+++ src/main/java/com/klu/JFSDS25_HQLProduct/Employee.java
@@ -0,0 +1,27 @@
+package com.klu.JFSDS25_HQLProduct;
+
+public class Employee {
+	private int pid;
+	private String pname;
+	private double pcost;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public double getPcost() {
+		return pcost;
+	}
+	public void setPcost(double pcost) {
+		this.pcost = pcost;
+	}
+	
+
+}
---
+++ src/main/java/com/klu/JFSDS25_HQLProduct/InsertHiber.java
@@ -0,0 +1,9 @@
+package com.klu.JFSDS25_HQLProduct;
+
+public class InsertHiber {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
